811172e6f4d73af4d3038fb80cc4d829c5e9ca53
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-04 05:05:47 +0530

    :)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,7 +44,7 @@ function Header() {
           {
             isAuthenticated && <>
             <button className='hidden lg:flex shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] cursor-pointer justify-center items-center rounded-full w-8 h-8 text-center font-medium'><FcBusinessman className='text-2xl' /></button>
-            <button className='hidden lg:flex items-center justify-center shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] cursor-pointer text-center bg-gray-600 px-3 h-8 rounded-md text-sm text-neutral-100 fira-code-one'>Premium</button></>
+            </>
           }
 
           <div>
