c0da432b2fcb467e8db2dbec0eb0c142edd59f53
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-07 16:34:23 +0530

    graph view ogg tracking

--- src/config/siteConfig.tsx
+++ src/config/siteConfig.tsx
@@ -6,6 +6,7 @@ interface SiteConfig {
 export const siteConfig : SiteConfig[] = [
   { "id": 1, "name": "Explore", "path": "/explore" },
   { "id": 2, "name": "Problems", "path": "/problems" },
+  { "id": 33, "name": "Track Progress", "path": "/progress" },
   { "id": 10, "name": "Building", "path": "/dev" },
   { "id": 3, "name": "Contest", "path": "/contest" },
   { "id": 5, "name": "Interview", "path": "/interview" },
