c46cbdbd67fd74a75e2bc4da334167daf80b84fe
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-11 05:02:30 +0530

    Now code secession start

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -18,33 +18,91 @@ import { FcCheckmark } from "react-icons/fc";
 import { IoClipboardOutline } from "react-icons/io5";
 import Calendar1 from '@/components/Code/Calendar'
 import CompanyPremiumProblemsTags from '@/components/Code/CompanyPremiumProblemsTags'
+import { BsBuildings } from "react-icons/bs";
+import { MdDateRange } from "react-icons/md";
+import { FiSidebar } from "react-icons/fi";
+import { MdOutlineClose } from "react-icons/md";
+
+import { BiLogoGraphql } from "react-icons/bi";
+import { IoChevronUpSharp } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
+import GraphView from '@/components/Code/GraphView'
 
 function Problems() {
+
   const [isTags , setIsTags] = useState(false)
   const [solvedList , setSolvedList] = useState(false)
   const [favorite , setFavorite] = useState(false)
   const [solved , setSolved] = useState(false)
 
+  const [showAll, setShowAll] = useState(false)
+  const [showCompaniesPremiumProblems, setShowCompaniesPremiumProblems] = useState(false)
+  const [showCalendar, setShowCalendar] = useState(false)
+  const [showGraph, setShowGraph] = useState(false)
+
+
 
   return (
     <div className='mx-auto' >
         <Header />
-        <div className='py-16 main-container md:h-svh h-[100%] grid grid-cols-1 lg:grid-cols-6'>
+        <div className='py-16 main-container md:h-svh h-svh grid grid-cols-1 lg:grid-cols-6 relative'>
 
-    
-            <div className='hidden pb-20 h-svh bg-neutral-100 dark:bg-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] border-r-1 relative lg:grid lg:grid-rows-3 gap-1 px-2 '>
+          {/*  For small screens */}
 
-              <div className='rounded-xl bg-neutral-100 dark:bg-neutral-800 row-span-2 mt-2 w-full h-fullshadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] p-2'>
+            <div className='z-40 lg:hidden  fixed right-2 bottom-2 flex flex-col gap-1'>
+
+              {showAll ? <span className='text-2xl mx-2' onClick={() => setShowAll(!showAll)}><IoIosArrowDown/></span>: <span className='mx-2 text-2xl' onClick={() => setShowAll(!showAll)}><IoChevronUpSharp/></span>}
+
+              { showAll && <>
+              <div onClick={() => setShowGraph(!showGraph)} className='text-neutral-200 bg-neutral-950 p-3 rounded-full text-xl shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'><BiLogoGraphql /></div>
+
+              <div onClick={() => setShowCalendar(!showCalendar)} className='text-neutral-200 bg-neutral-950 p-3 rounded-full text-xl shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'><MdDateRange /></div></>}
+              
+              <div className='p-3 rounded-full bg-neutral-950 text-white text-xl shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'><FiSidebar /></div>
+
+              <div onClick={() => setShowCompaniesPremiumProblems(!showCompaniesPremiumProblems)} className='bg-[linear-gradient(90deg,hsla(33,100%,53%,1)_0%,hsla(58,100%,68%,1)_100%)] p-3 rounded-full text-xl shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'><BsBuildings /></div>
+
+            </div>
+              
+            
+            { showGraph && <div className='absolute left-1/2 top-1/2 w-full h-auto transform -translate-x-1/2 -translate-y-1/2 z-50 px-6'>
+                <div className='relative py-2'>
+                  <span className='absolute bottom-0 right-2 top-4 z-50 text-xl' onClick={() => setShowGraph(false)}><MdOutlineClose/></span>
+                  <GraphView />
+                </div>
+              </div>
+            }
+
+            { showCompaniesPremiumProblems && <div className='absolute left-1/2 top-1/2 w-full h-auto transform -translate-x-1/2 -translate-y-1/2 z-50 px-6'>
+                <div className='relative py-2'>
+                  <span className='absolute bottom-0 right-2 top-4 z-50 text-xl' onClick={() => setShowCompaniesPremiumProblems(false)}><MdOutlineClose/></span>
+                  <CompanyPremiumProblemsTags />
+                </div>
+              </div>
+            }
+
+            { showCalendar && <div className='absolute left-1/2 top-1/2 w-full h-auto transform -translate-x-1/2 -translate-y-1/2 z-50 px-6'>
+                <div className='relative py-2'>
+                  <span className='absolute bottom-0 right-2 top-4 z-50 text-xl' onClick={() => setShowCalendar(false)}><MdOutlineClose/></span>
+                  <Calendar1 />
+                </div>
+              </div>
+            }
+
+
+            <div className={`${showCompaniesPremiumProblems || showCalendar || showGraph ? `blur-sm`: `blur-none`} hidden pb-20 h-svh bg-neutral-100 dark:bg-neutral-900 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] border-r-1 relative lg:grid lg:grid-rows-3 gap-1 px-2 `}>
+
+              <div className='rounded-xl bg-neutral-100 dark:bg-neutral-800 row-span-2 mt-2 w-full h-full shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] p-2'>
                 
               </div>
 
-              <div className='rounded-xl bg-neutral-100 dark:bg-neutral-800 mt-2 w-full h-full  shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] flex justify-center items-center p-4'>
-                <h2 className='text-2xl font-sans font-medium text-neutral-700'>Graph View</h2>
+              <div>
+                <GraphView/>
               </div>
 
             </div>
 
-            <div className='lg:px-10 pb-20 overflow-y-scroll lg:col-span-4 w-full h-auto bg-neutral-100 dark:bg-neutral-900    shadow-   [0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] border-r-1 mx-auto py-0'>
+            <div className={`${showCompaniesPremiumProblems || showCalendar || showGraph ? `blur-sm`: `blur-none`} lg:px-10 pb-20 overflow-y-scroll lg:col-span-4 w-full h-auto bg-neutral-100 dark:bg-neutral-900    shadow-   [0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] border-r-1 mx-auto py-0`}>
 
               <AttendenceSystem />
               <div className='px-2 py-6'>
@@ -56,7 +114,7 @@ function Problems() {
               <div className='w-full grid grid-cols-1 md:grid-cols-2 justify-center items-center px-4 md:px-4 pt-3'>
                 <div className='relative lg:order-none md:col-span-1 md:order-none order-2  md:px-4 lg:px-4'>
                   <span className='absolute left-3 md:left-7 lg:left-7 top-3 text-xl text-neutral-800 dark:text-neutral-200'><IoMdSearch /></span>
-                  <input className='w-full px-9 py-2 rounded-full bg-neutral-200 dark:bg-neutral-800 font-sana text-md outline-none' type="text" placeholder='search problem:- '/>
+                  <input className='z-20 w-full px-9 py-2 rounded-full bg-neutral-200 dark:bg-neutral-800 font-sana text-md outline-none' type="text" placeholder='search problem:- '/>
                 </div>
                   
                 <div className='w-full flex items-center justify-between px-3 pt-2 pb-2'>
