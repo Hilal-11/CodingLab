103e87ae9f48af40e795049c28878de79559a478
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-10 15:29:30 +0530

    ...

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -13,10 +13,17 @@ import { LuClipboardPenLine } from "react-icons/lu";
 import { FaStar } from "react-icons/fa";
 import { HiOutlineStar } from "react-icons/hi2";
 import { problemsDataList } from '@/config/platformConfig/ProblemsList'
+
+import { FcCheckmark } from "react-icons/fc";
+import { IoClipboardOutline } from "react-icons/io5";
+import Calendar1 from '@/components/Code/Calendar'
+import CompanyPremiumProblemsTags from '@/components/Code/CompanyPremiumProblemsTags'
+
 function Problems() {
   const [isTags , setIsTags] = useState(false)
   const [solvedList , setSolvedList] = useState(false)
   const [favorite , setFavorite] = useState(false)
+  const [solved , setSolved] = useState(false)
 
 
   return (
@@ -29,7 +36,7 @@ function Problems() {
 
             </div>
 
-            <div className='pb-20 overflow-y-scroll lg:col-span-4 w-full h-auto bg-neutral-100 dark:bg-neutral-900    shadow-   [0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] border-r-1 mx-auto py-0'>
+            <div className='lg:px-10 pb-20 overflow-y-scroll lg:col-span-4 w-full h-auto bg-neutral-100 dark:bg-neutral-900    shadow-   [0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] border-r-1 mx-auto py-0'>
 
               <AttendenceSystem />
               <div className='px-2 py-6'>
@@ -68,19 +75,19 @@ function Problems() {
                 {
                   problemsDataList.map((list) =>(
                      list?.data?.questions.map((question) => (
-                        <div key={question.id} className='flex items-center justify-between bg-neutral-200 dark:bg-neutral-800 py-2 rounded-xl px-1 w-full'>
+                        <div key={question.id} className='border flex items-center justify-between bg-neutral-200 dark:bg-neutral-800 py-2 rounded-xl px-1 w-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'>
                           <div className='flex items-center '>
                             {
-                              solvedList ? (<span onClick={() => setSolvedList(!solvedList)} className=' cursor-pointer inline-block p-2 bg-neutral-200 dark:bg-neutral-800 rounded-full font-sans text-lg font-medium text-neutral-800 dark:text-neutral-200 '><IoMdDoneAll /></span>) : (<span onClick={() => setSolvedList(!solvedList)} className=' cursor-pointer inline-block p-2 bg-neutral-200 dark:bg-neutral-800 rounded-full font-sans text-lg font-medium text-neutral-800 dark:text-neutral-200 '><LuClipboardPenLine /></span>)
+                              solved ? (<span className=' cursor-pointer inline-block p-2 bg-neutral-200 dark:bg-neutral-800 rounded-full font-sans text-lg font-medium text-blue-600 '><FcCheckmark /></span>) : (<span className=' cursor-pointer inline-block p-2 bg-neutral-200 dark:bg-neutral-800 rounded-full font-sans text-lg font-medium text-neutral-800 dark:text-neutral-200 '><IoClipboardOutline /></span>)
                             }
-                            <p className='max-w-[120px] text-nowrap md:max-w-[400px] overflow-hidden  px-2 md:pl-2 font-sans text-xs lg:text-[16px] font-medium text-neutral-800 dark:text-neutral-200'>{question.title}</p>
+                            <p className='max-w-[120px] text-nowrap md:max-w-[400px] overflow-hidden  px-1 md:pl-2 font-sans text-xs lg:text-[16px] font-medium text-neutral-800 dark:text-neutral-200'>{question.title}</p>
                             
 
                           </div>
                           
                           <div className='lg:px-4 gap-2 flex lg:gap-10 md:gap-10 items-center'>
-                            <p className='text-md font-sans text-neutral-400'>{question.acRate.toFixed(3)}</p>
-                            <p className={`text-md font-sans ${
+                            <p className='text-xs font-semibold font-sans text-neutral-400'>{question.acRate.toFixed(3)}</p>
+                            <p className={`text-sm font-medium font-sans ${
                                 question.difficulty.toLocaleLowerCase() === "easy"
                                   ? "text-teal-500"
                                   : question.difficulty.toLocaleLowerCase() === "medium"
@@ -92,25 +99,25 @@ function Problems() {
                             {
                               favorite ? (<span onClick={() => setFavorite(!favorite)} className=' cursor-pointer inline-block p-2 bg-neutral-200 dark:bg-neutral-800 rounded-full font-sans text-xl font-medium text-neutral-800 dark:text-neutral-200 '><FaStar /></span>) : (<span onClick={() => setFavorite(!favorite)} className=' cursor-pointer inline-block p-2 bg-neutral-200 dark:bg-neutral-800 rounded-full font-sans text-xl font-medium text-neutral-800 dark:text-neutral-200 '><HiOutlineStar /></span>)
                             }
+
+                            
+                                      
                           </div>
-                          <div className='flex gap-3'>
-                              {
-                                question?.topicTags.map((topicTags, index) => (
-                                  <span className='text-xs px-3 font-sans font-medium py-px rounded-full bg-neutral-100 dark:bg-neutral-900' key={index}>{topicTags.name}</span>
-                                ))
-                              }
-                            </div>
                         </div>
+                        
                      ))
                   ))
                 }
               </div>
-
-
-
             </div>
 
             <div className="pb-20 hidden h-auto  lg:flex flex-col gap-2 z-20 bg-neutral-100 dark:bg-neutral-900    shadow-   [0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
+              <div className='p-2'>
+                <Calendar1 />
+              </div>
+              <div className='p-2'>
+                <CompanyPremiumProblemsTags />
+              </div>
             </div>
 
 
