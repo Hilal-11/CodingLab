97e110d7bddf4af07fa142df35917dc59ae7a2d6
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-11 05:02:57 +0530

    Now code secession start

---
+++ src/components/Code/GraphView.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GraphView() {
+  return (
+              <div className='rounded-xl bg-neutral-100 dark:bg-neutral-800 mt-2 w-full h-full  shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] flex justify-center items-center p-4'>
+                <h2 className='text-2xl font-sans font-medium text-neutral-700'>Graph View</h2>
+              </div>
+  )
+}
+
+export default GraphView
