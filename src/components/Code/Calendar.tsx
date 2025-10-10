e6ea5c0cdcc079230f092206b1101216638c15f3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-11 05:02:43 +0530

    Now code secession start

--- src/components/Code/Calendar.tsx
+++ src/components/Code/Calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-
+import { cn } from "@/lib/utils"
 import { Calendar } from "@/components/ui/calendar"
 
 
@@ -19,8 +19,15 @@ function Calendar1() {
         onSelect={setDate}
         className="w-full bg-neutral-100 dark:bg-neutral-800 rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]"
       />
-      <div className="w-full h-[100px] rounded-xl bg-[linear-gradient(90deg,hsla(33,100%,53%,1)_0%,hsla(58,100%,68%,1)_100%)]  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-
+      <div className="bg-neutral-100 dark:bg-neutral-900 w-full h-[100px] rounded-xl shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] relative">
+        <div
+          className={cn(
+              "h-full md:h-full absolute inset-0",
+              "[background-size:5px_5px]",
+              "[background-image:linear-gradient(to_right,rgba(228,228,231,0.7)_1px,transparent_1px),linear-gradient(to_bottom,rgba(228,228,231,0.7)_1px,transparent_1px)]",
+              "dark:[background-image:linear-gradient(to_right,rgba(38,38,38,0.7)_1px,transparent_1px),linear-gradient(to_bottom,rgba(38,38,38,0.7)_1px,transparent_1px)]"
+            )}
+          />
       </div>
     </div>
   )
